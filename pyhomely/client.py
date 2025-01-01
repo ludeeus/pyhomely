578e025f2523c042e121633c91f7dde0da686a02
ludeeus <joasoe@gmail.com>
2025-01-01 16:37:02 +0000

    Fix

--- pyhomely/client.py
+++ pyhomely/client.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 
 import asyncio
 from datetime import datetime, timezone
-from typing import TYPE_CHECKING, Any, Awaitable, cast
+from typing import TYPE_CHECKING, Any, Awaitable, Mapping, cast
 
 import aiohttp
+import aiohttp.typedefs
 import socketio
 
 from .const import API_HOST
@@ -77,7 +78,7 @@ class ApiClient:
         method: str = "GET",
         *,
         params: list[tuple[str, str | int]] | None = None,
-        headers: dict[str, str] | None = None,
+        headers: Mapping[str, str] | None = None,
         data: Any | None = None,
         timeout: float = 10.0,
         force_new_token: bool = False,
@@ -85,22 +86,23 @@ class ApiClient:
     ) -> Any:
         """Call the API endpoint and return the response."""
         access_token = None if force_new_token else await self._get_access_token()
+        headers = {
+            aiohttp.hdrs.CONTENT_TYPE: "application/json",
+            aiohttp.hdrs.ACCEPT: "application/json",
+            **(
+                {aiohttp.hdrs.AUTHORIZATION: f"Bearer {access_token}"}
+                if access_token
+                else {}
+            ),
+            **(headers or {}),
+        }
         try:
             async with self._client_session.request(
                 method=method,
                 url=f"https://{API_HOST}/homely/{endpoint}",
                 params=params,
                 json=data,
-                headers={
-                    aiohttp.hdrs.CONTENT_TYPE: "application/json",
-                    aiohttp.hdrs.ACCEPT: "application/json",
-                    **(
-                        {aiohttp.hdrs.AUTHORIZATION: f"Bearer {access_token}"}
-                        if access_token
-                        else {}
-                    ),
-                    **(headers or {}),
-                },
+                headers=headers,
                 timeout=aiohttp.ClientTimeout(total=timeout),
             ) as response:
                 json_response: Any = None
